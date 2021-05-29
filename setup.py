ba12e2844eecfb27b12e2b50915c4d6434354793
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2021-05-29 05:32:29 -0400

    logging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requires = ["pip","ffmpeg-python"]
 setup(
     name='radio',
     version='0.1',
-    py_modules=['radio','downloader'],
+    py_modules=['radio','downloader','logger'],
     entry_points={
         'console_scripts': ['radio = radio:run']
     },
