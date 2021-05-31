72706c2b6aa1bb9632c358cd4f0c3af38781a892
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2021-05-31 17:01:42 -0400

    make "nix run" work again

--- radio.py
+++ radio.py
@@ -3,33 +3,27 @@ import uploader
 import transcoder
 from time import sleep
 
-def cb():
-  global d
-  global t
-  print("----------------FINISHED-----------")
-  t.listener.blockUntilEmpty()
-  u.listener.blockUntilEmpty()
-  d.stop()
-  t.stop()
-  d = downloader.Downloader('https://www.youtube.com/watch?v=BQQ3qZ9FC70', cb)
+def run():
+  def cb():
+    global d
+    global t
+    global u
+    print("----------------FINISHED-----------")
+    t.listener.blockUntilEmpty()
+    u.listener.blockUntilEmpty()
+    d.stop()
+    t.stop()
+    d = downloader.Downloader('https://www.youtube.com/watch?v=BQQ3qZ9FC70', cb)
+    t = transcoder.Transcoder(d)
+    u.setUpstream(t)
+
+  u = uploader.Uploader()
+  d = downloader.Downloader('https://www.youtube.com/watch?v=kgBcg4uBd9Q', cb)
   t = transcoder.Transcoder(d)
   u.setUpstream(t)
 
-u = uploader.Uploader()
-d = downloader.Downloader('https://www.youtube.com/watch?v=kgBcg4uBd9Q', cb)
-t = transcoder.Transcoder(d)
-u.setUpstream(t)
-
-#def run():
-  # up = uploader.Uploader()
-  # t = transcoder.Transcoder(up)
-  # downloader.download('https://www.youtube.com/watch?v=BaW_jenozKc', t.callback)
-  # t = transcoder.Transcoder(up)
-  # downloader.download('https://www.youtube.com/watch?v=EbnH3VHzhu8', t.callback)
-  # downloader.download('https://www.youtube.com/watch?v=kgBcg4uBd9Q', cb)
-  # downloader.download('https://www.youtube.com/watch?v=EbnH3VHzhu8', cb)
-
-if __name__ == "__main__":
-  #run()
   while True:
     sleep(1)
+
+if __name__ == "__main__":
+  run()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requires = ["pip","ffmpeg-python"]
 setup(
     name='radio',
     version='0.1',
-    py_modules=['radio','downloader','uploader','logger','util'],
+    py_modules=['radio','downloader','uploader','logger','util','stream_listener','transcoder'],
     entry_points={
         'console_scripts': ['radio = radio:run']
     },
