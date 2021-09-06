092cbb824d96b71002250b6ee823d5452163ce2b
zuckerberg <5-zuckerberg@users.noreply.git.neet.dev>
2021-09-06 14:18:40 -0400

    opus test

--- nullsrc.py
+++ nullsrc.py
@@ -9,7 +9,7 @@ class NullSrc(StreamSource):
   def __init__(self):
     self.process = ( ffmpeg
       .input('anullsrc', format='lavfi')
-      .output('pipe:', format='mp3')
+      .output('pipe:', format='opus')
       .run_async(pipe_stdout=True, pipe_stderr=True)
     )
     logger.add(self.process.stderr, "nullsrc.log")
